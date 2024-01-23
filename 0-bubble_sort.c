b0b78cbdf71f791206f2d1688e9669fbc0224317
taf <muzirat0@gmail.com>
2024-01-23 22:18:29 +0200

    bubble update
    cleane code
    documentation
    print arr after each swap

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,39 +1,33 @@
 #include "sort.h"
 
-void swap(int *arr, size_t size);
-void bubble_helper(int *array, size_t size);
-
+/**
+ * bubble_sort - sorting algorithm
+ * @array: Input, array to sort
+ * @size: Input, array len
+ *
+ * Return, none
+ */
 void bubble_sort(int *array, size_t size)
 {
-	bubble_helper(array, size);
-}
-
-void swap(int *arr, size_t size)
-{
-	size_t i, tmp, idx0, idx1;
-
-	for (i = 0; i < size - 1; i++)
-	{
-		idx0 = i;
-		idx1 = i + 1;
-		if (arr[idx0] > arr[idx1])
-		{
-			tmp = arr[idx0];
-			arr[idx0] = arr[idx1];
-			arr[idx1] = tmp;
-		}
-	}
-}
-
-void bubble_helper(int *array, size_t size)
-{
-	size_t i;
+	size_t i, j, tmp, idx0, idx1;
 
 	if (size < 2)
 		return;
 	for (i = 0; i < size; i++)
 	{
-		swap(array, size);
-		print_array(array, size);
+
+		for (j = 0; j < size - 1; j++)
+		{
+			idx0 =j;
+			idx1 = j + 1;
+			if (array[idx0] > array[idx1])
+			{
+				tmp = array[idx0];
+				array[idx0] = array[idx1];
+				array[idx1] = tmp;
+				print_array(array, size);
+			}
+		}
 	}
 }
+
